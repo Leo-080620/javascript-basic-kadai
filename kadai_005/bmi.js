c8e4ff6b08d2cd5076efcefd9ab8f36cb5b24b93
Leo-080620 <ko.011020.ki@gmail.com>
2026-04-21 11:10:52 +0900

    Add files via upload

--- kadai_005/bmi.js
+++ kadai_005/bmi.js
@@ -4,5 +4,8 @@ let weight = 68;
 // 身長の変数の宣言・代入
 let height = 1.7;
 
+// BMIの変数の宣言・代入
+let bmi = weight / (height * height);
+
 // 計算結果の出力
-console.log(weight / (height * height));
+console.log(bmi);
